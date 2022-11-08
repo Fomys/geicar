32d0f339db8d79ef3d9a6d6345a68f011efab6af
augalloo <augustingalloo@gmail.com>
2022-11-08 17:03:55 +0100

    Node asservissement

---
+++ raspberryPI3/ros2_ws/src/asservissement/include/direction_control/direction_control_node.h
@@ -0,0 +1,25 @@
+//
+// Created by andrea on 07/11/22.
+//
+
+#ifndef DIRECTION_CONTROL_DIRECTION_CONTROL_NODE_H
+#define DIRECTION_CONTROL_DIRECTION_CONTROL_NODE_H
+
+#include "rclcpp/rclcpp.hpp"
+#include <chrono>
+#include <functional>
+#include <memory>
+
+#include "interfaces/msg/motors_order.hpp"
+#include "interfaces/msg/motors_feedback.hpp"
+#include "interfaces/msg/steering_calibration.hpp"
+#include "interfaces/msg/joystick_order.hpp"
+#include "interfaces/msg/gains.hpp"
+
+#include "std_srvs/srv/empty.hpp"
+
+
+
+
+
+#endif //DIRECTION_CONTROL_DIRECTION_CONTROL_NODE_H
