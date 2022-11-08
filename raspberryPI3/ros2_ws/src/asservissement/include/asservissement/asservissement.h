cefddc261d071c975a3c4d75f15ee00e1b4aa503
andreapefe <andreaperezfdez@gmail.com>
2022-11-08 20:29:07 +0100

    Node asservissement to test in real

--- raspberryPI3/ros2_ws/src/asservissement/include/asservissement/asservissement.h
+++ raspberryPI3/ros2_ws/src/asservissement/include/asservissement/asservissement.h
@@ -5,17 +5,23 @@
 #ifndef DIRECTION_CONTROL_DIRECTION_CONTROL_NODE_H
 #define DIRECTION_CONTROL_DIRECTION_CONTROL_NODE_H
 
+/*
+ * Includes for cpp file
+ */
+
+//General
 #include "rclcpp/rclcpp.hpp"
 #include <chrono>
 #include <functional>
 #include <memory>
 
+//Messages
 #include "interfaces/msg/motors_order.hpp"
 #include "interfaces/msg/motors_feedback.hpp"
-#include "interfaces/msg/steering_calibration.hpp"
-#include "interfaces/msg/joystick_order.hpp"
-#include "interfaces/msg/gains.hpp"
+#include "interfaces/msg/motors_feedback.hpp"
+#include "interfaces/msg/angle_order.hpp"
 
+//STD
 #include "std_srvs/srv/empty.hpp"
 
 
--- raspberryPI3/ros2_ws/src/asservissement/src/asservissement.cpp
+++ raspberryPI3/ros2_ws/src/asservissement/src/asservissement.cpp
@@ -1,4 +1,4 @@
-#include "../include/direction_control/direction_control_node.h"
+#include "../include/asservissement/asservissement.h"
 
 using namespace std;
 using placeholders::_1;
@@ -12,21 +12,24 @@ public:
         //Parameter declaration for gains in PID
         auto param_desc = rcl_interfaces::msg::ParameterDescriptor{}; //description of param
         param_desc.description = "Gain Kp";
-        this->declare_parameter("kp", 1, param_desc);
+        this->declare_parameter("kp", 1.0, param_desc);
 
         //Set value of the parameter for use
-        int kp = this->get_parameter("kp").get_parameter_value();
+        Kp = this->get_parameter("kp").get_parameter_value().get<float>();
 
-        //Publisher sur le topic commande des moteurs
-        publisher_can_= this->create_publisher<interfaces::msg::MotorsOrder>("motors_order", 10);
+        //Publishers
+        // sur le topic commande des moteurs
+        publisher_can_ = this->create_publisher<interfaces::msg::MotorsOrder>("motors_order", 10);
 
-        //Subscription aux données des moteurs
+        //Subscriptions
+        // Données des moteurs
         subscription_motors_feedback_ = this->create_subscription<interfaces::msg::MotorsFeedback>(
-                "motors_feedback", 10, std::bind(&car_control::motorsFeedbackCallback, this, _1));
+                "motors_feedback", 10, std::bind(&asservissement::motorsFeedbackCallback, this, _1));
 
-        //Subscription to the users input
+//        //Users input
         subscription_brain_order_ = this->create_subscription<interfaces::msg::AngleOrder>(
-                "brain_order", 10, std::bind(&car_control::executeBrainCmd, this, _1));
+                "brain_order", 10, std::bind(&asservissement::executeAngleCmd, this, _1));
+
 
         //Inform the log the node has been launched
         RCLCPP_INFO(this->get_logger(), "direction_control_node READY");
@@ -34,29 +37,46 @@ public:
 
 private:
 
+    //Publishers
+    rclcpp::Publisher<interfaces::msg::MotorsOrder>::SharedPtr publisher_can_;
+
+    //Subscriptions
+    rclcpp::Subscription<interfaces::msg::MotorsFeedback>::SharedPtr subscription_motors_feedback_;
+    rclcpp::Subscription<interfaces::msg::AngleOrder>::SharedPtr subscription_brain_order_;
+
     //Requested angle by brain
     int requestedSteerAngle = 0;
 
     //Data for motor feedback
     uint8_t steeringPwmCmd;
 
-    //Execute the Cmd of the brain when an order is sent
-    void executeBrainCmd(const interfaces::msg::MotorsOrder & order)
+    //Data about PID corrector
+    int Kp = 1;
+
+    /*
+     * Callback to execute the angle cmd of the brain when order is sent
+     */
+    void executeAngleCmd(const interfaces::msg::AngleOrder & angle)
     {
-        RCLCPP_INFO(this->get_logger(), "Valeur Kp : %d", this->get_parameter("kp").get_parameter_value());
+        RCLCPP_INFO(this->get_logger(), "Valeur Kp : %d", Kp);
     }
 
+    /*
+     * Callback function when feedback topic is updated
+     */
+    void motorsFeedbackCallback(const interfaces::msg::MotorsFeedback & motorsFeedback)
+    {
 
-
-
-
+    }
 
 };
 
+
+//Running node
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<car_control>();
+    auto node = std::make_shared<asservissement>();
 
     rclcpp::spin(node);
 
