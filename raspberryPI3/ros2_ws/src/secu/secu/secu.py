065852fd0e5b2f6b914de9ec5bbbe8d7d5551586
Minh Hieu Bui <mhbui@insa-toulouse.fr>
2022-11-13 19:04:09 +0100

    Nettoyage secu nickel

--- raspberryPI3/ros2_ws/src/secu/secu/secu.py
+++ raspberryPI3/ros2_ws/src/secu/secu/secu.py
@@ -1,85 +1,55 @@
 import rclpy
 from rclpy.node import Node
 
-from interfaces.msg import MotorsOrder
 from interfaces.msg import StopCar
 from interfaces.msg import SpeedOrder
 from interfaces.msg import SpeedInput
 
+
 class Security(Node):
+    STOP_SPEED = 0.0  # RPM
+    CAUTIOUS_SPEED_REAR = -30.0  # RPM
+    CAUTIOUS_SPEED_FRONT = 30.0  # RPM
 
     def __init__(self):
         super().__init__('secu')
 
-        #Variables
+        # Variables
         self.stop_ = StopCar()
-        self.motors_order_ = MotorsOrder()
-        self.speed_ = SpeedOrder()
 
-        #Publishers
-        #publish informations to MotorsOrder topic
-        self.publisher_can_ = self.create_publisher(MotorsOrder, 'motors_order', 10)
+        # Publishers
+        # Publish to SpeedOrder
         self.publisher_speed_order_ = self.create_publisher(SpeedOrder, 'speed_order', 10)
 
-        #Subscribers
-        #subscribe to StopCar topic
+        # Subscribers
+        # subscribe to StopCar topic
         self.subscription_stop_car_ = self.create_subscription(StopCar, 'stop_car', self.stop_car_callback, 10)
-        self.subscription_stop_car_
 
-        #subscribe to SpeedOrder topic
-        self.subscription_speed_input_ = self.create_subscription(SpeedInput, 'speed_input', self.speed_input_callback, 10)
-        self.subscription_speed_input_
+        # subscribe to SpeedOrder topic
+        self.subscription_speed_input_ = self.create_subscription(SpeedInput, 'speed_input', self.speed_input_callback,
+                                                                  10)
 
-        #initiate StopCar msg array
-        self.stop_car_msg = [-1, -1, -1]
+    def stop_car_callback(self, msg: StopCar):
+        # stock StopCar msg value in array
+        self.stop_ = msg
 
-    def stop_car_callback(self,msg):
-        #stock StopCar msg value in array
-        self.stop_car_msg[0] = msg.stop_car
-        self.stop_car_msg[1] = msg.slow_rear
-        self.stop_car_msg[2] = msg.slow_front
+    def speed_input_callback(self, speed_input: SpeedInput):
+        # Variables
+        speed_ = SpeedOrder()
+        speed_.speed_order = speed_input.speed_order_input
 
-        self.get_logger().info(f'Stop Car: {self.stop_car_msg[0]}')
-        self.get_logger().info(f'Slow Rear: {self.stop_car_msg[1]}')
-        self.get_logger().info(f'Slow Front: {self.stop_car_msg[2]}')
+        self.get_logger().info(f'Desired input speed: {speed_input.speed_order_input}')
 
-
-        # if self.stop_car_msg[0] == true:
-        #     self.motors_order_.left_rear_pwm = 50
-        #     self.motors_order_.right_rear_pwm = 50
-        #     self.publisher_can_.publish(self.motors_order_)
-        #
-        # if self.motors_order_[1] == true:
-        #     self.motors_order_.left_rear_pwm = 30
-        #     self.motors_order_.right_rear_pwm = 30
-        #     self.publisher_can_.publish(self.motors_order_)
-        #
-        # if self.motors_order_[2] == true:
-        #     self.motors_order_.left_rear_pwm = 70
-        #     self.motors_order_.right_rear_pwm = 70
-        #     self.publisher_can_.publish(self.motors_order_)
-
-    def speed_input_callback(self, speedinput):
-        self.get_logger().info(f'Desired input speed: {speedinput.speed_order_input}')
-
-        if self.stop_car_msg[0] == True:
-            self.speed_.speed_order = 0.0 #RPM
-            self.publisher_speed_order_.publish(self.speed_)
-
-        elif self.stop_car_msg[1] == True:
-            if speedinput.speed_order_input <= -30.0:
-                self.speed_.speed_order = max(speedinput.speed_order_input, -30.0) #RPM
-                self.publisher_speed_order_.publish(self.speed_)
-
-        elif self.stop_car_msg[2] == True:
-            if speedinput.speed_order_input >= 30.0:
-                self.speed_.speed_order = min(speedinput.speed_order_input, 30.0) #RPM
-                self.publisher_speed_order_.publish(self.speed_)
+        if self.stop_.stop_car:
+            speed_.speed_order = self.STOP_SPEED
 
         else:
-#if (self.stop_car_msg[0] == False) and (self.stop_car_msg[1] == False) and  (self.stop_car_msg[2] == False):
-            self.speed_.speed_order = speedinput.speed_order_input
-            self.publisher_speed_order_.publish(self.speed_)
+            if self.stop_.slow_rear:
+                speed_.speed_order = max(speed_input.speed_order_input, self.CAUTIOUS_SPEED_REAR)  # RPM
+            if self.stop_.slow_front:
+                speed_.speed_order = min(speed_input.speed_order_input, self.CAUTIOUS_SPEED_FRONT)  # RPM
+
+        self.publisher_speed_order_.publish(speed_)
 
 
 def main(args=None):
@@ -88,6 +58,7 @@ def main(args=None):
     rclpy.spin(secu)
     secu.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
