2c02a61247e6789a6f5938e25614bccb1281ccdd
Minh Hieu Bui <mhbui@insa-toulouse.fr>
2022-11-13 18:33:49 +0100

    Nettoyage obstacle detection

--- raspberryPI3/ros2_ws/src/obs_detect/obs_detect/obstacle_detection.py
+++ raspberryPI3/ros2_ws/src/obs_detect/obs_detect/obstacle_detection.py
@@ -4,97 +4,52 @@ from rclpy.node import Node
 from interfaces.msg import Ultrasonic
 from interfaces.msg import MotorsOrder
 from interfaces.msg import StopCar
+
+
 class ObstacleDetection(Node):
+    MINIMAL_DISTANCE = 20
+    CAUTION_DISTANCE = 50
 
     def __init__(self):
         super().__init__('obstacle_detection')
 
-        #Variables
-        self.motors_order_ = MotorsOrder()
-        self.stop_ = StopCar()
+        # Publishers
+        # publish informations to StopCar topic
+        self.publish_stop_car = self.create_publisher(StopCar, 'stop_car', 10)
 
-        #Publishers
-        #publish informations to MotorsOrder topic
-        self.publisher_can_ = self.create_publisher(MotorsOrder, 'motors_order', 10)
-        #publish informations to StopCar topic
-        self.stop_car_ = self.create_publisher(StopCar,'stop_car', 10)
+        # Subscribers
+        self.subscription_us = self.create_subscription(Ultrasonic, 'us_data', self.us_callback, 10)
 
-        #time_period = 0.05
-        #self.timer1 = self.create_timer(time_period, self.timer_callback1)
-        #self.i = 0
+    def us_callback(self, msg: Ultrasonic):
+        stop = StopCar()
+        stop.slow_front = False
+        stop.slow_rear = False
+        stop.stop_car = False
 
-        #self.distance_ = self.create_publisher(Ultrasonic, 'distance', 10)
-        #publish to Brain
-        #time_period = 0.05
-        #self.timer2  = self.create_timer(time_period, self.timer_callback2)
+        if msg.front_left < self.CAUTION_DISTANCE \
+                or msg.front_center < self.CAUTION_DISTANCE \
+                or msg.front_right < self.CAUTION_DISTANCE:
+            stop.slow_front = True
 
-        #Subscribers
-        self.subscription_us_ = self.create_subscription(Ultrasonic, 'us_data', self.us_callback, 10)
-        # subscription to Ultrasound Data topic
-        self.subscription_us_
+        if msg.rear_left < self.CAUTION_DISTANCE \
+                or msg.rear_center < self.CAUTION_DISTANCE \
+                or msg.rear_right < self.CAUTION_DISTANCE:
+            stop.slow_rear = True
 
-        #initiate Ultrasound Distance array to stock value
-        self.usDistance = [-1, -1, -1, -1, -1, -1]
+        if msg.front_left < self.MINIMAL_DISTANCE \
+                or msg.front_center < self.MINIMAL_DISTANCE \
+                or msg.front_right < self.MINIMAL_DISTANCE \
+                or msg.rear_left < self.MINIMAL_DISTANCE \
+                or msg.rear_center < self.MINIMAL_DISTANCE \
+                or msg.rear_right < self.MINIMAL_DISTANCE:
+            stop.slow_front = True
 
-    #def timer_callback(self):
-
-
-    def us_callback(self, msg):
-        self.usDistance[0] = msg.front_left
-        self.usDistance[1] = msg.front_center
-        self.usDistance[2] = msg.front_right
-        self.usDistance[3] = msg.rear_left
-        self.usDistance[4] = msg.rear_center
-        self.usDistance[5] = msg.rear_right
-
-#        self.get_logger().info('Distance front left: "%i" cm' % self.usDistance[0])
-#        self.get_logger().info('Distance front center: "%i" cm' % self.usDistance[1])
-#        self.get_logger().info('Distance front right: "%i" cm' % self.usDistance[2])
-#        self.get_logger().info('Distance rear left: "%i" cm' % self.usDistance[3])
-#        self.get_logger().info('Distance rear center: "%i" cm' % self.usDistance[4])
-#        self.get_logger().info('Distance rear right: "%i" cm' % self.usDistance[5])
-
-        #self.motors_order_.left_rear_pwm = 100
-        #self.motors_order_.right_rear_pwm = 100
-
-                #if self.motors_order_.left_rear_pwm > 70:
-                #        self.motors_order_.left_rear_pwm = 70
-
-                #if self.motors_order_.left_rear_pwm < 30:
-                #       self.motors_order_.left_rear_pwm = 30
-
-                #if self.motors_order_.right_rear_pwm > 70:
-                #        self.motors_order_.right_rear_pwm = 70
-
-                #if self.motors_order_.right_rear_pwm < 30:
-                #        self.motors_order_.right_rear_pwm = 30
-        self.stop_.slow_front = False
-        self.stop_.slow_rear  = False
-        self.stop_.stop_car = False
-
-        for i in range(0, 3):
-            if self.usDistance[i] < 50:
-                self.stop_.slow_front = True
-
-        for i in range(3, 6):
-            if self.usDistance[i] < 50:
-                self.stop_.slow_rear = True
-
-        for i in range (0, 6):
-            if self.usDistance[i] < 20:
-                self.stop_.stop_car = True
-
-        self.stop_car_.publish(self.stop_)
-
-        # self.publisher_can_.publish(self.motors_order_)
-        self.get_logger().info(f'Publishing to secu node: {self.stop_}')
-        #self.i += 1
-
+        self.publish_stop_car.publish(stop)
 
 
 def main(args=None):
     rclpy.init(args=args)
-    obstacle_detection =ObstacleDetection()
+    obstacle_detection = ObstacleDetection()
     rclpy.spin(obstacle_detection)
     obstacle_detection.destroy_node()
     rclpy.shutdown()
