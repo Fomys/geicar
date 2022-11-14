24134c1d4d96c154088b242e543bcf37802215cb
andreapefe <andreaperezfdez@gmail.com>
2022-11-14 16:29:56 +0100

    debug add

--- raspberryPI3/ros2_ws/src/secu/secu/secu.py
+++ raspberryPI3/ros2_ws/src/secu/secu/secu.py
@@ -40,6 +40,8 @@ class Security(Node):
 
         self.get_logger().info(f'Desired input speed: {speed_input.speed_order_input}')
 
+        self.get_logger().info(f'Stop : {self.stop_.stop_car}')
+
         if self.stop_.stop_car:
             speed_.speed_order = self.STOP_SPEED
             self.get_logger().info('STOP!!!!')
