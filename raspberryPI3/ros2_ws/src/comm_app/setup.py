bd1f5f9af6d50a29858c32334943149fc64b30b5
TshirC <jeoannel@outlook.com>
2023-01-07 16:16:56 +0100

    commApp test

--- raspberryPI3/ros2_ws/src/comm_app/setup.py
+++ raspberryPI3/ros2_ws/src/comm_app/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-                "test_comm = comm_app.comm_app:main"
+            "comm_app = comm_app.comm_app:main"
         ],
     },
 )
