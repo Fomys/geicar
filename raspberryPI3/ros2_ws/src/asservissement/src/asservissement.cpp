dfa5698f657854e7baad4c254a4a3c7cbfcb4cb3
Madeliine <yuxna.ma@gmail.com>
2023-01-08 13:33:51 +0100

    Test nouveau PID qui compile

--- raspberryPI3/ros2_ws/src/asservissement/src/asservissement.cpp
+++ raspberryPI3/ros2_ws/src/asservissement/src/asservissement.cpp
@@ -31,17 +31,17 @@ public:
 
         //Parameter declaration for gains in PID
         //PID of the motor of the left wheel
-        this->declare_parameter("kp_l", 1.0);
-        this->declare_parameter("ki_l", 0.0);
+        this->declare_parameter("kp_l", 0.8);
+        this->declare_parameter("ki_l", 1000.0);
         this->declare_parameter("kd_l", 0.0);
         //PID of the motor of the right wheel
-        this->declare_parameter("kp_r", 1.0);
-        this->declare_parameter("ki_r", 0.0);
+        this->declare_parameter("kp_r", 0.8);
+        this->declare_parameter("ki_r", 900.0);
         this->declare_parameter("kd_r", 0.0);
         //PID of the motor of direction
-        this->declare_parameter("kp_s", 1.0);
-        this->declare_parameter("ki_s", 1.0);
-        this->declare_parameter("kd_s", 1.0);
+        //this->declare_parameter("kp_s", 1.0);
+        //this->declare_parameter("ki_s", 1.0);
+        //this->declare_parameter("kd_s", 1.0);
 
         //Set value of the parameter for use
         Kp_l = this->get_parameter("kp_l").get_parameter_value().get<float>();
