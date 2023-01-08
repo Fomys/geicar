69dff43652b7872bbd7eba5696893db11945b2f1
TshirC <jeoannel@outlook.com>
2023-01-08 18:12:47 +0100

    test comm_app subscriber

--- raspberryPI3/ros2_ws/src/comm_app/comm_app/comm_app.py
+++ raspberryPI3/ros2_ws/src/comm_app/comm_app/comm_app.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 #from interfaces.msg import MessageApp
 #from interfaces.msg import Package
 from interfaces.msg import MotorsFeedback
-import lgpio
+#import lgpio
 #import time
 
 #import numpy as np
 class CommApp(Node):
 
     #Class constants
-    GPIO_HANDLE = 0
+    #GPIO_HANDLE = 0
     GPIO_PIN = 23
     #duration = 1   # 1 sec of duration
 
@@ -28,20 +29,20 @@ class CommApp(Node):
 
 
     def listener_callback(self, msg: MotorsFeedback):
-        #self.get_logger().info('I am in front of the door: "%s"' % msg.data)
         self.get_logger().info(str(msg))
+        #self.get_logger().info('I am in front of the door: "%s"' % msg.data)
         #m = MessageApp()
         #m.detectDoor = True  #Car in front of the door
-        self.lgpio.buzzer = lgpio.gpiochip_open(self.GPIO_HANDLE)
+        #self.lgpio.buzzer = lgpio.gpiochip_open(self.GPIO_HANDLE)
 
-        lgpio.gpio_write(self.buzzer, self.GPIO_PIN, 1)
-        self.lgpio.sleep(5)
-        lgpio.gpio_write(self.buzzer, self.GPIO_PIN, 0)
+        #lgpio.gpio_write(self.buzzer, self.GPIO_PIN, 1)
+        #self.lgpio.sleep(5)
+        #lgpio.gpio_write(self.buzzer, self.GPIO_PIN, 0)
 
         #lgpio.gpio_claim_output(self.buzzer, self.GPIO_PIN, 1, lFlags=0)
 
 
-def main(args=None):
+def main():
     rclpy.init()
 
     comm_app = CommApp()
