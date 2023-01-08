1bb6148a009f11388142ba33bee04bfc98cb353b
TshirC <jeoannel@outlook.com>
2023-01-08 17:09:04 +0100

    récup project

--- raspberryPI3/ros2_ws/src/asservissement/src/asservissement.cpp
+++ raspberryPI3/ros2_ws/src/asservissement/src/asservissement.cpp
@@ -341,8 +341,8 @@ private:
         leftRearPwmCmd = leftPwmCmd;
         rightRearPwmCmd = rightPwmCmd;
 
-        RCLCPP_INFO(this->get_logger(), "Left pwm %f", leftRearPwmCmd);
-        RCLCPP_INFO(this->get_logger(), "right pwm %f", rightRearPwmCmd);
+        RCLCPP_INFO(this->get_logger(), "Left pwm %d", leftRearPwmCmd);
+        RCLCPP_INFO(this->get_logger(), "right pwm %d", rightRearPwmCmd);
     }
 
     void asservSteering ()
